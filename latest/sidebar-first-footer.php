5cc0ab67dd12052d4a7883b3a336a12046ec56b7
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-10-17 16:23:07 -0500

    Sync with developemnt

--- latest/sidebar-first-footer.php
+++
@@ -1,4 +0,0 @@
- 
-	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	<?php endif; ?>
--- tags/1.1.0/content-3.php
+++
@@ -1,4 +0,0 @@
-<article>
-    <?php the_post_thumbnail('small-thumb'); ?>
-    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-</article>
